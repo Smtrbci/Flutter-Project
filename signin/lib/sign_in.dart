9571e5491f0a5fcb960aaad142eb711bf8ba5034
Smtrbci <75490736+Smtrbci@users.noreply.github.com>
2022-04-19 16:04:14 +0300

    TextField

--- signin/lib/sign_in.dart
+++ signin/lib/sign_in.dart
@@ -13,15 +13,105 @@ class _SignInState extends State<SignIn> {
     return Container(
       child: Scaffold(
         appBar: AppBar(
-            backgroundColor: Colors.white,
-            elevation: 0,
-            leading: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.chevron_left,
-                color: Colors.tealAccent[400],
+          backgroundColor: Colors.white,
+          elevation: 0,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.chevron_left,
+              color: Colors.tealAccent[400],
+            ),
+          ),
+        ),
+        body: Column(
+          children: [
+            Container(
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: const Text(
+                  "Sign In",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                ),
               ),
-            )),
+            ),
+            const Expanded(
+              flex: 0,
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Optional Group Special Code",
+                    filled: true,
+                    fillColor: Color.fromARGB(179, 200, 200, 200),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(30),
+                      ),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const Expanded(
+              flex: 0,
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Email Address",
+                    filled: true,
+                    fillColor: Color.fromARGB(179, 200, 200, 200),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(30),
+                      ),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const Expanded(
+              flex: 0,
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    filled: true,
+                    fillColor: Color.fromARGB(179, 200, 200, 200),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(30),
+                      ),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              child: Row(
+                children: const [
+                  Expanded(
+                    flex: 3,
+                    child: Text(
+                      "Stay Logged In",
+                    ),
+                  ),
+                  Expanded(
+                    flex: 0,
+                    child: Text(
+                      "Forgot Your Password ?",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ],
+        ),
+        backgroundColor: Colors.white,
       ),
     );
   }
