2479afa63ff1d46dde4355059c85d07c081e1338
Smtrbci <75490736+Smtrbci@users.noreply.github.com>
2022-04-19 15:02:54 +0300

    AppBar

--- signin/lib/sign_in.dart
+++ signin/lib/sign_in.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
-  const SignIn({ Key? key }) : super(key: key);
+  const SignIn({Key? key}) : super(key: key);
 
   @override
   State<SignIn> createState() => _SignInState();
@@ -11,7 +11,18 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.white,
+            elevation: 0,
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.chevron_left,
+                color: Colors.tealAccent[400],
+              ),
+            )),
+      ),
     );
   }
 }
