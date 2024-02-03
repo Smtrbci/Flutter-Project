e01df137bfb0eb92099ec1ea4f61481a10745213
Samet Arabacı <75490736+Smtrbci@users.noreply.github.com>
2024-02-04 02:31:02 +0300

    04.02.2024

--- bmi_calculator_app/lib/veiws/bmi_veiws.dart
+++ bmi_calculator_app/lib/veiws/bmi_veiws.dart
@@ -17,24 +17,37 @@ class _BMIVeiwsState extends State<BMIVeiws> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Vücut Kitle Endeksi Hesaplama'),
-        backgroundColor: const Color.fromARGB(190, 91, 136, 103),
+        title: const Text(
+          'Vücut Kitle İndeksi Hesaplama',
+          style: TextStyle(
+            color: Color.fromARGB(200, 32, 46, 95),
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(200, 166, 201, 199),
         centerTitle: true,
         elevation: 0,
       ),
-      backgroundColor: const Color.fromARGB(255, 182, 214, 185),
+      backgroundColor: Color.fromARGB(220, 166, 201, 199),
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            
+            Image.asset("assets/vkii.png"),
+            const SizedBox(
+              height: 20,
+            ),
             TextField(
               controller: _heightController,
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
-                labelText: 'CM cinsinden yükseklik',
+                labelText: 'CM Cinsinden Yükseklik',
+                labelStyle: TextStyle(color: Colors.black),
                 icon: Image.asset("assets/height.png"),
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 0.0),
+                ),
               ),
             ),
             const SizedBox(
@@ -44,8 +57,13 @@ class _BMIVeiwsState extends State<BMIVeiws> {
               controller: _weightController,
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
-                labelText: 'KG cinsinden ağırlık',
+                labelText: 'KG Cinsinden Ağırlık',
+                labelStyle: TextStyle(color: Colors.black),
                 icon: Image.asset("assets/weight.png"),
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 0.0),
+                ),
               ),
             ),
             const SizedBox(
@@ -55,23 +73,39 @@ class _BMIVeiwsState extends State<BMIVeiws> {
               //color: Colors.pinkAccent,
               style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.all(
-                      const Color.fromARGB(190, 91, 136, 103))),
+                      const Color.fromARGB(200, 166, 201, 199))),
+              onPressed: calculateBMI,
               child: const Text(
                 "Hesapla",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color.fromARGB(200, 32, 46, 95),
+                ),
               ),
-              onPressed: calculateBMI,
             ),
             const SizedBox(
-              height: 15,
+              height: 20,
             ),
-            Text(
+            Container(
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1.2,
+                ),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Text(
                 _result == null
                     ? "Değer Girin"
                     : "${_result?.toStringAsFixed(2)}",
-                style: const TextStyle(
+                style: TextStyle(
                   color: Colors.black,
-                  fontSize: 40,
-                )),
+                  fontSize: 35,
+                  fontWeight: _result == null ? FontWeight.normal : FontWeight.normal,
+                ),
+              ),
+            )
           ],
         ),
       ),
