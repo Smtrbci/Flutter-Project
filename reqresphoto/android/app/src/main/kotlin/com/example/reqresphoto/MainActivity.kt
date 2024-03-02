2525d6ff44a590b3f9be88e385c74e4bb6e0cae2
Samet Arabacı <75490736+Smtrbci@users.noreply.github.com>
2024-03-03 00:35:21 +0300

    Delete reqresphoto directory

--- reqresphoto/android/app/src/main/kotlin/com/example/reqresphoto/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.reqresphoto
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- reqresphoto/lib/photo/photos.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import '../view/reqres_photo_view.dart';
-
-class Photo extends StatefulWidget {
-  const Photo({Key? key}) : super(key: key);
-
-  @override
-  ReqresPhotoView createState() => ReqresPhotoView();
-}
