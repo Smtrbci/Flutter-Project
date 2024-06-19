4960592601b83efdcac5311d59ac9107d3242f8e
Samet Arabacı <75490736+Smtrbci@users.noreply.github.com>
2024-06-19 16:34:04 +0300

    19.06.2024

--- todo_mlpcare_app/lib/main.dart
+++ todo_mlpcare_app/lib/main.dart
@@ -15,16 +15,9 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(),
-      home: HomeView(),
-=======
     AppRouter appRouter = AppRouter();
     return MaterialApp.router(
       routerConfig: appRouter.config(),
->>>>>>> 4ec1c1d2a1d6c39259d02c182568c12b742d3de4
     );
   }
 }
